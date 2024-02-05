5f562041241401fcbf692910527e7405f15b1b64
Tanaboon <darkhikaru@gmail.com>
2024-02-05 20:41:23 +0700

    Update
    Update integrator

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def renderBlinn():
     main_camera.aspect_ratio = 16.0/9.0
     main_camera.img_width = 320
     main_camera.center = rtu.Vec3(0,0,0)
-    main_camera.samples_per_pixel = 100
+    main_camera.samples_per_pixel = 30
     main_camera.max_depth = 5
     main_camera.vertical_fov = 60
     main_camera.look_from = rtu.Vec3(-2, 2, 1)
@@ -29,9 +29,9 @@ def renderBlinn():
 
     mat_tex_checker_bw = rtm.TextureColor(tex_checker_bw)
 
-    mat_blinn1 = rtm.Blinn(rtu.Color(0.8, 0.5, 0.8), 0.5, 0.2, 8)
+    mat_blinn1 = rtm.Blinn(rtu.Color(0.8, 0.5, 0.8), 0.5, 0.2, 0.08)
     mat_blinn2 = rtm.Blinn(rtu.Color(0.4, 0.5, 0.4), 0.5, 0.6, 8)
-    mat_blinn3 = rtm.Blinn(rtu.Color(0.8, 0.5, 0.4), 0.5, 0.2, 8)
+    mat_blinn3 = rtm.Blinn(rtu.Color(0.8, 0.5, 0.4), 0.5, 0.2, 300)
 
 
     world = rts.Scene()
@@ -46,11 +46,11 @@ def renderBlinn():
     intg = rti.Integrator(bSkyBG=True)
 
     renderer = rtren.Renderer(main_camera, intg, world)
-    # renderer.render()
-    # renderer.write_img2png('week09_nojitter_fs5.png')    
+    renderer.render()
+    renderer.write_img2png('week09_nojittered.png')    
 
-    renderer.render_jittered()
-    renderer.write_img2png('week09_jitter_fs5_07.png')    
+    # renderer.render_jittered()
+    # renderer.write_img2png('week09_jitter_fs5_07.png')    
 
 
 if __name__ == "__main__":
